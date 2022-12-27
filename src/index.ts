c60b3687bfe43d6d20a3a7ac813bdfad008e83ae
Elad Ben-Israel <elad.benisrael@gmail.com>
2022-12-27 18:46:34 +0200

    accept .d.ts file directly

--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,17 @@ import fs from 'fs';
 import path from 'path';
 import { Parser } from './decl';
 
-const packageName = process.argv[2];
-if (!packageName) {
-  throw new Error('No package specified');
+const dtsFile = process.argv[2];
+if (!dtsFile) {
+  console.error(`Usage: ${path.basename(process.argv[1])} <package-name>`);
+  process.exit(1);
 }
 
-const dir = require.resolve(`${packageName}/package.json`);
-const pkg = JSON.parse(fs.readFileSync(dir, 'utf8'));
-const filepath = path.join(path.dirname(dir), pkg.types);
-const parser = new Parser(filepath);
+const parser = new Parser(dtsFile);
 const types = parser.parse();
 
-fs.writeFileSync(`${path.basename(packageName)}.types.json`, JSON.stringify(types, undefined, 2));
+const outfile = `${path.basename(path.dirname(dtsFile))}.types.json`;
 
+fs.writeFileSync(outfile, JSON.stringify(types, undefined, 2));
+
+console.log(outfile);
